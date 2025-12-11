cbc26adfb4d3de418cb464aebacc599a7a2017da
dimapWKV <dimapn3000@gmail.com>
2025-12-11 23:24:47 +0700

    [wikivik] feat (keyboard_utils): инлайн-клавиатуры для интерфейса бота
    - Клавиатура запуска диалога (start_dialog)
    - Клавиатура помощи (help_button)
    - Клавиатура ответов Да/Нет (yes/No)
    - Использование локализованных текстов из лексикона

--- keyboards/keyboard_utils.py
+++ keyboards/keyboard_utils.py
@@ -1,6 +1,30 @@
-def main():
-    pass
+from lexicon.lexicon_ru import RU
 
+from aiogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 
-if __name__ == '__main__':
-    main()
+# Создаем объекты инлайн-кнопок
+button_1 = InlineKeyboardButton(text=RU["start_dialog"],
+                                callback_data="start_dialog")
+# Создаем объекты инлайн-кнопок
+button_help = InlineKeyboardButton(text=RU["help_button"],
+                                   callback_data="help_button")
+
+# Создаем объекты инлайн-кнопок
+button_answer_yes = InlineKeyboardButton(text="Да", callback_data="yes")
+
+# Создаем объекты инлайн-кнопок
+button_answer_no = InlineKeyboardButton(text="Нет", callback_data="No")
+
+# Создаем объект инлайн-клавиатуры
+keyboard_start_dialog = InlineKeyboardMarkup(inline_keyboard=[[button_1]])
+
+# Создаем объект инлайн-клавиатуры
+keyboard_help_dialog = InlineKeyboardMarkup(inline_keyboard=[[button_help]])
+
+# Создаем объект инлайн-клавиатуры
+keyboard_answer = InlineKeyboardMarkup(
+    inline_keyboard=[[button_answer_yes, button_answer_no]])
